76396d9d6a0eed9ccfc9abff9d54e6ddb752eb94
SHWEROY_T3DT <shweroy@apac.corpdir.net>
2024-10-11 00:05:07 +0530

    load data

--- src/CardList.jsx
+++ src/CardList.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const CardList = () => {
+  const [characters, setCharacters] = useState([]);
+  const [pageInfo, setPageInfo] = useState(null);
+
+  useEffect(()=>{
+    const baseUrl = "https://rickandmortyapi.com/api"
+    async function fetchData() {
+      await fetch(`${baseUrl}/character`)
+      .then(res => res.json())
+      .then(data => {
+        setCharacters(prev => [...data.results])
+        setPageInfo(data.info)
+      })
+    }
+    fetchData();
+  },[])
+
+  console.log(pageInfo)
   return (
-    <div>CardList</div>
+    <div>
+      {characters.map(character => <p key={character.id}>{character.name}</p>)}
+    </div>
   )
 }
 
